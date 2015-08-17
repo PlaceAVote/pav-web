e649136113769fbec14662d0559dc88d3444b85d
Anthony O'Neill <anthony@atostudios.co.uk>
2015-08-17 17:30:35 +0100

    added new transition styles to login page

--- js/login.js
+++ js/login.js
@@ -3,36 +3,36 @@ $( document ).ready(function() {
 
     $('.email-login').on('click', function() {
     	$('.vanish').toggleClass('visible');
-    })
+    });
 
-	$( "form" ).on( "submit", function( event ) {
-	  event.preventDefault();
-	  console.log( $( this ).serialize() );
-	  var loginData = $( this ).serialize();
-	  var loginUrl = "http://pav-user-api-1996773369.us-east-1.elb.amazonaws.com:8080/user/authenticate";
+	// $( "form" ).on( "submit", function( event ) {
+	//   event.preventDefault();
+	//   console.log( $( this ).serialize() );
+	//   var loginData = $( this ).serialize();
+	//   var loginUrl = "http://pav-user-api-1996773369.us-east-1.elb.amazonaws.com:8080/user/authenticate";
 	  	
-	  $.ajax({
-	  		type: "POST",
-	  		url: loginUrl,
-	  		data: {"email":"tony@place.com", "password" : "stuff"},
-	  		dataType: "json",
-	  		crossDomain: true,
-	  		headers: {"Content-Type": "application/json", "Accept": "application/json"},
-	  		beforeSend: function (xhr) {
-            xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-       		 },
-	  		success: function(data) {
-	  			console.log('success');
-	  			console.log(data);
-	  		},
-	  		error: function(data) {
-	  			console.log('error');
-	  			console.log(data);
-	  		}
-	  });
+	//   $.ajax({
+	//   		type: "POST",
+	//   		url: loginUrl,
+	//   		data: {"email":"tony@place.com", "password" : "stuff"},
+	//   		dataType: "json",
+	//   		crossDomain: true,
+	//   		headers: {"Content-Type": "application/json", "Accept": "application/json"},
+	//   		beforeSend: function (xhr) {
+ //            xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+ //       		 },
+	//   		success: function(data) {
+	//   			console.log('success');
+	//   			console.log(data);
+	//   		},
+	//   		error: function(data) {
+	//   			console.log('error');
+	//   			console.log(data);
+	//   		}
+	//   });
 
 
-	});
+	// });
 
 
 });
