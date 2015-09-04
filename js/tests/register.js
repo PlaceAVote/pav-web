ab2e732161e9880725cb3e2b53f84522baf45d44
Anthony O'Neill <anthony.thomas.oneill@gmail.com>
2015-09-04 20:03:39 +0100

    try out of automated testing prior to pushing to master

--- js/tests/register.js
+++ js/tests/register.js
@@ -16,33 +16,33 @@ describe('Controller: LoginCtrl', function() {
 
 
 		it('Should make sure the email fail if not valid', function() {
-			scope.login.user.email[0] = "anthony.com";
+			scope.login.user.email = "anthony.com";
 			scope.login.validate(scope.login.user);
-			expect(scope.login.user.email[1]).toBe(false);
+			expect(scope.login.user.emailValid).toBe(false);
 		});
 
 
 
 		it('Should make sure the password fails client side validation', function() {
-			scope.login.user.email[0] = "anthony@email.com";
-			scope.login.user.password[0] = "password";
+			scope.login.user.email = "anthony@email.com";
+			scope.login.user.password = "password";
 			scope.login.validate(scope.login.user);
-			expect(scope.login.user.password[1]).toBe(false);
+			expect(scope.login.user.passwordValid).toBe(false);
 		});
 
 		it('Should fail client side validation for password and email', function() {
-			scope.login.user.email[0] = "anthonyemail.com";
-			scope.login.user.password[0] = "password";
+			scope.login.user.email = "anthonyemail.com";
+			scope.login.user.password = "password";
 			scope.login.validate(scope.login.user);
-			expect(scope.login.user.email[1]).toBe(false);
-			expect(scope.login.user.password[1]).toBe(false);
+			expect(scope.login.user.emailValid).toBe(false);
+			expect(scope.login.user.passwordValid).toBe(false);
 		});
 
 		it('Should allow valid email address and password to pass client side validation', function() {
-			scope.login.user.email[0] = "anthony@email.com";
-			scope.login.user.password[0] = "p455worD";
+			scope.login.user.email = "anthony@email.com";
+			scope.login.user.password = "p455worD";
 			scope.login.validate(scope.login.user);
-			expect(scope.login.user.email[1]).toBe(true);
-			expect(scope.login.user.password[1]).toBe(true);
+			expect(scope.login.user.emailValid).toBe(true);
+			expect(scope.login.user.passwordValid).toBe(true);
 		});
 });
