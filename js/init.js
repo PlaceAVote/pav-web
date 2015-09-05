0172c209cabfcfe8f35886a0c96fbe5b97982562
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-05 20:16:39 +0100

    remove console log

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 var RegisterController = require('./Controllers/registerController.js');
 var SignUpController = require('./Controllers/userRegisterController.js');
 var LoginController = require('./Controllers/loginController.js');
-console.log(LoginController);
 
 var angular = require('angular');
 var app = angular.module('pavApp', [require('angular-route'), require('angular-animate'), require('angular-resource')]);
