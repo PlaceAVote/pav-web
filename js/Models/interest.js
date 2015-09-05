819eeb907bb8fe843409ace9c1409dcce83627a7
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-05 12:44:13 +0100

    update interest

--- js/Models/interest.js
+++ js/Models/interest.js
@@ -2,9 +2,10 @@ function Interest(name, icon){
 	this.name = name;
 	this.icon = icon;
 	this.selected = false;
-	this.select = function(){
-		this.selected = this.selected ? false : true;
-	}
+}
+
+Interest.prototype.select = function() {
+	this.selected = this.selected ? false : true;
 }
 
 module.exports = Interest;
