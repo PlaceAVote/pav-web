1c6a09f45d35ba0ce16a2f3c5b24b332a5be042c
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-05 11:51:01 +0100

    Add interest model

--- js/Models/interest.js
+++ js/Models/interest.js
@@ -2,6 +2,9 @@ function Interest(name, icon){
 	this.name = name;
 	this.icon = icon;
 	this.selected = false;
+	this.select = function(){
+		this.selected = this.selected ? false : true;
+	}
 }
 
 module.exports = Interest;
--- test/Models/interest.js
+++ test/Models/interest.js
@@ -14,5 +14,17 @@ describe("Interest", function(){
 		var subject = new Interest();
 		expect(subject.selected).to.eql(false);	
 	});
-
-});
+	it("changes the state of selected to true, when calling select (initial state of selected is false)", function(){
+                var subject = new Interest();
+                expect(subject.selected).to.be.false;
+                subject.select();
+                expect(subject.selected).to.be.true;
+        });
+        it("changes selected state to false, when was previously true", function(){
+                var subject = new Interest();
+                subject.selected = true;
+                expect(subject.selected).to.be.true;
+                subject.select();
+                expect(subject.selected).to.be.false;
+        });
+})
