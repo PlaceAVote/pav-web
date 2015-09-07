8387c4ba4d1fe9d2c910dbbdb0c79602f69e0ae4
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-07 23:44:04 +0100

    merge

--- js/init.js
+++ js/init.js
@@ -1,15 +1,8 @@
 var RegisterController = require('./Controllers/registerController.js');
-<<<<<<< HEAD
 var SignUpController = require('./Controllers/sign_up_controller.js');
 var LoginController = require('./Controllers/loginController.js');
 var angular = require('angular');
 
-=======
-var SignUpController = require('./Controllers/userRegisterController.js');
-var LoginController = require('./Controllers/loginController.js');
-
-var angular = require('angular');
->>>>>>> 77969475ea6406e1e71e148881853a268dba5866
 var app = angular.module('pavApp', [require('angular-route'), require('angular-animate'), require('angular-resource')]);
 
 app.config(['$routeProvider', function($routeProvider) {
