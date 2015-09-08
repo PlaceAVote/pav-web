3bbbad2442e19e95b9c9910c423506c2fa8503d9
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-08 22:04:13 +0100

    refactor to user revealing module pattern instead of prototype

--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,31 +1,37 @@
 var User = require('../models/user.js');
 
 function UserService() {
-}
+	var createUser = function(username, password) {
+		this.user = new User(username, password);
+	};
 
-UserService.prototype.createUser = function(username, password) {
-	this.user = new User(username, password);	
-};
+	var getUser = function() {
+		return this.user;
+	};
 
-UserService.prototype.getUser = function(){
-	return this.user;
-};
+	var addInterests = function(interests) {
+		if(!this.user) {
+			return;
+		}
+		this.user.interests = interests;
+	};
 
-UserService.prototype.addInterests = function(interests){
-	if(!this.user) {
-		return;
-	}
-	this.user.interests = interests;
-};
-
-UserService.prototype.addAdditionalInformation = function(additionalInformation) {
-	if(!this.user){
-		return;
-	}
-	this.user.first_name = additionalInformation.first_name;
-	this.user.last_name = additionalInformation.last_name;
-	this.user.country_code = additionalInformation.country_code;
-	this.user.dob = additionalInformation.dob;
+	var addAdditionalInformation = function(additionalInformation){
+		if(!this.user){
+			return;
+		}
+		this.user.first_name = additionalInformation.first_name;
+		this.user.last_name = additionalInformation.last_name;
+		this.user.country_code = additionalInformation.country_code;
+		this.user.dob = additionalInformation.dob;
+	};
+	
+	return {
+		createUser : createUser,
+		getUser : getUser,
+		addInterests: addInterests,
+		addAdditionalInformation : addAdditionalInformation	
+	};
 };
 
 module.exports = UserService;
