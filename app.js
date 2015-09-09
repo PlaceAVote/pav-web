2753d5b9997fbdb4c146090f6a3688a1e253d78b
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-09 18:54:13 +0100

    add app.js file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var port = process.env.PORT || 3000;
 var app = express();
 app.use('/', express.static(__dirname));
-app.listen(8080, function() {
+app.listen(port, function() { 
 	console.log('listening');
 });
