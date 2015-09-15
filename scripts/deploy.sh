e9f5d692a17cbb1a9204cbe60461ae253a5e14b7
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-15 20:17:36 +0100

    add live config swap step

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "-- Building Web-app..."
 
+#rm /src/config/endpoints.js
+gulp live-config
 gulp browserify-web
 
 echo "-- Deploying to environment..."
@@ -19,10 +21,10 @@ zip -r ${file} ./* -x '*.git*' -x '*node_modules/*' -x '*scripts/*' -x '*test/*'
 
 
 echo "Uploading zip to S3"
-aws s3api put-object --bucket ${bucket} --key ${file} --body ${file}
+#aws s3api put-object --bucket ${bucket} --key ${file} --body ${file}
 
 echo "Creating Application Version Elasticbeanstalk Instance"
-aws --region ${reg} elasticbeanstalk create-application-version --application-name ${application} --version-label ${label} --source-bundle S3Bucket=${bucket},S3Key=${file} --auto-create-application 
+#aws --region ${reg} elasticbeanstalk create-application-version --application-name ${application} --version-label ${label} --source-bundle S3Bucket=${bucket},S3Key=${file} --auto-create-application 
 
 echo "Updating Environment"
-aws --region ${reg} elasticbeanstalk update-environment --environment-name ${application} --version-label ${label}
+#aws --region ${reg} elasticbeanstalk update-environment --environment-name ${application} --version-label ${label}
