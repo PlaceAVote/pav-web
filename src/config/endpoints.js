def3e2a0744dbb4a204b8ae8c9a047e1de74b585
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-16 19:37:56 +0100

    update configs

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,8 +1,15 @@
 module.exports = {
     users : {
-        endpoint : 'http://192.168.99.100:8080/user/',
-        create : {
-            method : 'PUT'
-        }
-    }
+                endpoint : 'http://192.168.99.100:8080/user/',
+                create : {
+                    method : 'PUT',
+                    headers: {
+                        'Content-Type' : "application/json"
+                    },
+                    withCredentials : false
+                },
+                addUserToken: function(token) {
+                                  this.create.headers['PAV-USER-AUTH'] = token;
+                              }
+            }
 }
