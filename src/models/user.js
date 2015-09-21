dfe0d8688f1e7f666d06b3ed43616e367b75f9ed
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-21 18:18:55 +0100

    remove user formatting method from user service

--- src/models/user.js
+++ src/models/user.js
@@ -13,4 +13,29 @@ User.prototype.setToken = function(token) {
     this.password = undefined;
 };
 
+
+User.prototype.getTopicNames = function() {
+    var names = [];
+    if(this.topics){
+        var topicLength = this.topics.length;
+        for(var i = topicLength-1; i>=0; i--) {
+            names.push(this.topics[i].name);
+        };
+    }
+    return names;
+};
+
+
+User.prototype.toBody = function() {
+    return {
+        first_name: this.first_name,
+            last_name: this.last_name,
+            country_code: this.country_code,
+            password: this.password,
+            dob: this.dob,
+            topics: this.getTopicNames(),
+            email: this.email
+    };
+};
+
 module.exports = User;
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -4,9 +4,11 @@ var strftime = require('strftime');
 var facebook = require('../integrations/facebook.js');
 
 function UserService($resource) {
+
     var loginWithFacebook = function(){
         var facebook = new Facebook();
-        facebook.login();
+        facebook.login(function(resource){
+        });
     };
 
     var createUser = function(username, password) {
@@ -33,19 +35,8 @@ function UserService($resource) {
 		this.user.country_code = additionalInformation.country_code;
 		this.user.dob = strftime('%m/%d/%Y', additionalInformation.dob);
 	};
-    
     var saveUser = function(callback){
         var that = this;
-        var extractTopicNames = function(topics){
-            var names = [];
-            if(topics){
-                var topicLength = topics.length;
-                for(var i = topicLength-1; i>=0; i--) {
-                    names.push(topics[i].name);
-                };
-            }
-            return names;
-        };
 
         var onLoad = function(user){
             that.user.setToken(user.token);
@@ -58,15 +49,7 @@ function UserService($resource) {
             return;
         }
         var saveUser = new $resource(config.users.create_endpoint, undefined, {create : config.users.create});
-        var toSave = {
-            first_name: this.user.first_name,
-            last_name: this.user.last_name,
-            country_code: this.user.country_code,
-            password: this.user.password,
-            dob: this.user.dob,
-            topics: extractTopicNames(this.user.topics),
-            email: this.user.email
-        };
+        var toSave = this.user.toBody();
         saveUser.create(toSave, onLoad, onError);
 
     };
