029035541341fc58b86e9db824c661ab136749d8
HistoireDeBabar <p.barber1990@gmail.com>
2015-09-29 19:22:34 +0100

    add bills to feed controller

--- src/controllers/feed_controller.js
+++ src/controllers/feed_controller.js
@@ -1,11 +1,16 @@
 var Banner = require('../models/banner.js');
 
-FeedController = function($scope, $location, userService) {
+FeedController = function($scope, $location, userService, billService) {
     $scope = $scope || {};
     $location = $location || {};
     var user = userService.getUser();
     $scope.user = user;
     $scope.banner = new Banner(user);
+    billService.get(user.email, function(err, response){
+        if(!err){
+            $scope.bills = response;
+        }
+    };
 };
 
 module.exports = FeedController;
