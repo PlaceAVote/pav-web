5a05be2cec925b2fb112cf90b7416c9dcf49b031
HistoireDeBabar <p.barber1990@gmail.com>
2015-10-20 20:04:31 +0100

    add dislike to service

--- test/services/comment_service.js
+++ test/services/comment_service.js
@@ -202,4 +202,78 @@ describe('Comment Service', function() {
       });
     });
   });
+  describe('Dislike', function() {
+    it('Takes a Comment Id', function(done) {
+      var subject = new CommentService();
+      subject.dislike(undefined, function(err, result) {
+        expect(err).to.eql('No Comment Id');
+        done();
+      });
+    });
+    it('Returns Error if user is not defined', function(done) {
+      var userService = {
+        getUser: function() {
+          return;
+        },
+      };
+      var subject = new CommentService(undefined, userService);
+      subject.dislike('CommentID', function(err, result) {
+        expect(err).to.eql('No User Defined');
+        done();
+      });
+    });
+    it('Returns Error if user email is not defined', function(done) {
+      var userService = {
+        getUser: function() {
+          return {
+            email: ''
+          };
+        },
+      };
+      var subject = new CommentService(undefined, userService);
+      subject.dislike('CommentID', function(err, result) {
+        expect(err).to.eql('No User Defined');
+        done();
+      });
+    });
+    it('Calls Resource with correct params', function(done) {
+      function  mockResource(url, params, method) {
+        expect(url).to.eql('http://pav-congress-api-196217309.us-west-2.elb.amazonaws.com:8080/comments/CommentID/dislike');
+        expect(params).to.eql(undefined);
+        expect(method.like.headers.Autherization).to.eql('PAV_AUTH_TOKEN TOKEN');
+        this.dislike = function(body, onLoad, onError) {
+          expect(body).to.eql({author: 'test@test.com'});
+          done();
+        }
+      }
+      var subject = new CommentService(mockResource, userService, authService);
+      subject.dislike('CommentID', function(err, result) {
+      });
+    });
+    it('Returns Error when server returns error', function(done) {
+      function  mockResource(url, params, method) {
+        this.dislike = function(body, onLoad, onError) {
+          return onError('Error');
+        }
+      }
+      var subject = new CommentService(mockResource, userService, authService);
+      subject.dislike('CommentID', function(err, result) {
+        expect(err).to.eql('Error');
+        done();
+      });
+    });
+    it('Returns result', function(done) {
+      function  mockResource(url, params, method) {
+        this.dislike = function(body, onLoad, onError) {
+          return onLoad('response');
+        }
+      }
+      var subject = new CommentService(mockResource, userService, authService);
+      subject.dislike('CommentID', function(err, result) {
+        expect(err).to.eql(undefined);
+        expect(result).to.eql(true);
+        done();
+      });
+    });
+  });
 });
