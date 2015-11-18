c6f9ab4bdb737cec3f24a678c83bc407926ccae1
HistoireDeBabar <p.barber1990@gmail.com>
2015-11-18 18:41:15 +0000

    remove elastic beanstallk deployment from app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.use('/', express.static(__dirname));
-app.listen(port, function() { 
-	console.log('listening');
+
+app.listen(port, function() {
+  console.log('listening');
 });
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,25 +12,6 @@ gulp app-min
 
 echo "-- Deploying to environment..."
 
-
-label=$(date +%s)
-file="pav-web-dev.zip"
-bucket="pav-web-app"
-application="pav-web-dev"
-reg="us-west-2"
-
-zip -r ${file} ./* -x '*.git*' -x '*node_modules/*' -x '*scripts/*' -x '*test/*' -x '*scss/*' -x '*src/*' -x '*gulpfile.js' -x '*README.md*' -x '*circle.yml*'
-
-
-echo "Uploading zip to S3"
-aws s3api put-object --bucket ${bucket} --key ${file} --body ${file}
-
-echo "Creating Application Version Elasticbeanstalk Instance"
-aws --region ${reg} elasticbeanstalk create-application-version --application-name ${application} --version-label ${label} --source-bundle S3Bucket=${bucket},S3Key=${file} --auto-create-application
-
-echo "Updating Environment"
-aws --region ${reg} elasticbeanstalk update-environment --environment-name ${application} --version-label ${label}
-
 echo "Deploying to Convox"
 curl -Ls https://install.convox.com/linux.zip > /tmp/convox.zip
 sudo unzip /tmp/convox.zip -d /usr/local/bin
