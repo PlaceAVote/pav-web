7153439d69ab0b5b797699868b463d9932b11433
atostudios <https://atostudios@github.com>
2015-12-08 11:30:17 +0000

    added a call of getUserProfile once login is successful to place user object into rootscope to ensure profile image is correct

--- src/controllers/sign_up_controller.js
+++ src/controllers/sign_up_controller.js
@@ -40,9 +40,10 @@ SignUpCtrl.prototype.test = function() {
             }
             else {
                 that.rs.loggedIn = true;
+                if(!user.img_url) {
                 user.img_url = 'img/header/generic-user.png';
+                }
                 that.rs.user = user;
-                console.log(that.rs.user);
                 that.location.path("/feed");
             }
         });
