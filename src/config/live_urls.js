c56546851bc926a3726a474851372c1b8cbb0518
johnervine@hotmail.com <johnervine@hotmail.com>
2015-12-16 15:24:24 +0000

    When connecting to the notification websocket in production, we must use wss instead of ws for encryption purposes.

--- src/config/live_urls.js
+++ src/config/live_urls.js
@@ -1,7 +1,7 @@
 var CONGRESS = 'https://congressapi.placeavote.com';
 var USER = 'https://userapi.placeavote.com';
 var VOTES = 'https://voteapi.placeavote.com';
-var NOTIFICATION = 'ws://userapi.placeavote.com';
+var NOTIFICATION = 'wss://userapi.placeavote.com';
 var FACEBOOKAPPID = '949301848442473';
 
 module.exports = {
