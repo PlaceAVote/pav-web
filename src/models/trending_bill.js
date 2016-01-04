4770ea47038fa63e1124e22f9c44e431edc0a07c
atostudios <https://atostudios@github.com>
2016-01-04 16:54:54 +0000

    added icons to trending bills and icon trending globaly header

--- src/models/trending_bill.js
+++ src/models/trending_bill.js
@@ -6,6 +6,7 @@ this.yesVote = options['yes-count'];
 this.official_title = options.official_title;
 this.short_title = options.short_title;
 this.summary = options.summary;
+this.getIcon(options);
 }
 
 TrendingBill.prototype.majorityVote = function() {
@@ -32,5 +33,37 @@ TrendingBill.prototype.goToPage = function (location) {
   location.path('bill/' + this.bill_id);
 };
 
+TrendingBill.prototype.getIcon = function(options) {
+    var that = this;
+    if(!options.subject) {
+        that.icon = 'icon-building';
+        return;
+    }
+    var i = options.subject;
+    if(i == 'Religion') {
+        that.icon = 'icon-religion';
+    }   
+    if(i == 'Drugs') {
+        that.icon = 'icon-smoking-area';
+    }
+    if(i == 'Defense') {
+        that.icon = 'icon-tank';
+    }    
+    if(i == 'Politics') {
+        that.icon = 'icon-building';
+    }
+    if(i == 'Gun Rights') {
+        that.icon = 'icon-gun';
+    }
+    if(i == 'Technology') {
+        that.icon = 'icon-ipad';
+    }
+    if(i == 'Economics') {
+        that.icon = 'icon-money';
+    }
+    if(i == 'Social Interest') {
+        that.icon = 'icon-police';
+    }
+}
 
 module.exports = TrendingBill;
