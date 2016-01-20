712cfb55d3cb5b70a77fb0da6da29ad7c19f22e0
topgiter <boazt2t@gmail.com>
2016-01-20 20:48:39 +0100

    Added issue service

---
+++ src/services/issue_service.js
@@ -0,0 +1,98 @@
+var config = require('../config/endpoints.js');
+
+function IssueService($resource, authService, callback) {
+  var saveIssue = function(issue, callback) {
+    if (!issue) {
+      callback('Issue is required');
+      return;
+    }
+
+    var token = authService.getAccessToken();
+    if (!token) {
+      callback('Token is needed to save a issue');
+      return;
+    }
+
+    var onLoad = function() {
+      callback();
+    };
+
+    var onError = function(err) {
+      callback(err);
+    };
+
+    var url = config.users.issue.endpoint;
+    config.methods.put.headers.Authorization = token;
+    var request = new $resource(url, undefined, {save: config.methods.put});
+
+    request.save(issue, onLoad, onError);
+  };
+
+  var setIssueResponse = function(issue_id, response_value, callback) {
+    if (!issue_id || !response_value) {
+      callback('Issue id and response value is required');
+      return;
+    }
+
+    var token = authService.getAccessToken();
+    if (!token) {
+      callback('Token is needed to set issue response');
+      return;
+    }
+
+    var onLoad = function(result) {
+      callback(undefined, result);
+    };
+
+    var onError = function(err) {
+      callback(err);
+    };
+
+    var url = config.users.issue.response(issue_id);
+    config.methods.post.headers.Authorization = token;
+    var request = new $resource(url, undefined, {setResponse: config.methods.post});
+
+    var body = {
+      issue_id: issue_id,
+      emotional_response: response_value,
+    };
+
+    request.setResponse(body, onLoad, onError);
+  };
+
+  var getIssueResponse = function(issue_id, callback) {
+    if (!issue_id) {
+      callback('Issue id is required');
+      return;
+    }
+
+    var token = authService.getAccessToken();
+    if (!token) {
+      callback('Token is needed to get issue response');
+      return;
+    }
+
+    var onLoad = function(result) {
+      callback(undefined, result);
+    };
+
+    var onError = function(err) {
+      callback(err);
+    };
+
+    var url = config.users.issue.response(issue_id);
+    config.methods.get.headers.Authorization = token;
+    var request = new $resource(url, undefined, {getResponse: config.methods.get});
+
+    request.getResponse(undefined, onLoad, onError);
+  };
+
+  return {
+    saveIssue: saveIssue,
+    setIssueResponse: setIssueResponse,
+    getIssueResponse: getIssueResponse,
+  };
+}
+
+module.exports = IssueService;
+
---
+++ test/services/issue_service.js
@@ -0,0 +1,215 @@
+var expect = require('chai').expect;
+var IssueService = require('../../src/services/issue_service.js');
+
+describe("Issue Service", function() {
+  var mockAuthWithoutToken = {
+    getAccessToken: function() {
+      return;
+    },
+  };
+
+  var mockAuth = {
+    getAccessToken: function() {
+      return 'Token';
+    },
+  };
+
+  var mockIssue = {
+    bill_id: "hr2-114",
+    comment: "Comment Body goes here",
+  };
+
+  describe("Save Issue", function() {
+    it('returns an error if no issue is given', function(done) {
+      var subject = new IssueService({}, {});
+      subject.saveIssue(undefined, function(err) {
+        expect(err).to.eql('Issue is required');
+        done();
+      });
+    });
+
+    it('requires a token to save a issue', function(done) {
+      var subject = new IssueService({}, mockAuthWithoutToken);
+      subject.saveIssue(true, function(err, result) {
+        expect(err).to.eql('Token is needed to save a issue');
+        done();
+      });
+    });
+
+    it('calls params with correct config', function(done) {
+      function mockResource(url, params, method) {
+        this.save = function(){};
+        expect(url).to.contain('/user/issue');
+        expect(params).to.eql(undefined);
+        expect(method.save.headers.Authorization).to.eql('Token');
+        expect(method.save.method).to.eql('PUT');
+        done();
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.saveIssue(function(err, result) {
+        expect(err).to.eql(undefined);
+      });
+    });
+
+    it('returns an error from the server', function(done) {
+      function mockResource(url, params, method) {
+        this.save = function(body, onLoad, onError) {
+          expect(body).to.eql(mockIssue);
+          onError('Error');
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.saveIssue(mockIssue, function(err) {
+        expect(err).to.eql('Error');
+        done();
+      });
+    });
+
+    it('returns no error when succesful put', function(done) {
+      function mockResource(url, params, method) {
+        this.save = function(body, onLoad, onError) {
+          expect(body).to.eql(mockIssue);
+          onLoad();
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.saveIssue(mockIssue, function(err) {
+        expect(err).to.eql(undefined);
+        done();
+      });
+    });
+  });
+
+  var mockIssueId = '010101';
+  var mockResponseValue = 1;
+
+  describe("Set Issue Response", function() {
+    it('returns an error if issue id or response value is not given', function(done) {
+      var subject = new IssueService({}, {});
+      subject.setIssueResponse(undefined, undefined, function(err) {
+        expect(err).to.eql('Issue id and response value is required');
+        done();
+      });
+    });
+
+    it('requires a token to response', function(done) {
+      var subject = new IssueService({}, mockAuthWithoutToken);
+      subject.setIssueResponse(mockIssueId, mockResponseValue, function(err, result) {
+        expect(err).to.eql('Token is needed to set issue response');
+        done();
+      });
+    });
+
+    it('calls params with correct config', function(done) {
+      function mockResource(url, params, method) {
+        this.setResponse = function(){};
+        expect(url).to.contain('/user/' + mockIssueId + '/response');
+        expect(params).to.eql(undefined);
+        expect(method.setResponse.headers.Authorization).to.eql('Token');
+        expect(method.setResponse.method).to.eql('POST');
+        done();
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.setIssueResponse(mockIssueId, mockResponseValue, function(err, result) {
+        expect(err).to.eql(undefined);
+      });
+    });
+
+    it('returns an error from the server', function(done) {
+      function mockResource(url, params, method) {
+        this.setResponse = function(body, onLoad, onError){
+          expect(body.issue_id).to.eql(mockIssueId);
+          expect(body.emotional_response).to.eql(mockResponseValue);
+          onError('Error');
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.setIssueResponse(mockIssueId, mockResponseValue, function(err, result) {
+        expect(err).to.eql('Error');
+        done();
+      });
+    });
+
+    it('returns no error when succesful put', function(done) {
+      function mockResource(url, params, method) {
+        this.setResponse = function(body, onLoad, onError) {
+          onLoad(body);
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.setIssueResponse(mockIssueId, mockResponseValue, function(err, result) {
+        expect(err).to.eql(undefined);
+        expect(result.issue_id).to.eql(mockIssueId);
+        expect(result.emotional_response).to.eql(mockResponseValue);
+        done();
+      });
+    });
+  });
+
+  describe("Get Issue Response", function() {
+    it('returns an error if issue id is not given', function(done) {
+      var subject = new IssueService({}, {});
+      subject.getIssueResponse(undefined, function(err) {
+        expect(err).to.eql('Issue id is required');
+        done();
+      });
+    });
+
+    it('requires a token to get response value', function(done) {
+      var subject = new IssueService({}, mockAuthWithoutToken);
+      subject.getIssueResponse(mockIssueId, function(err, result) {
+        expect(err).to.eql('Token is needed to get issue response');
+        done();
+      });
+    });
+
+    it('calls params with correct config', function(done) {
+      function mockResource(url, params, method) {
+        this.getResponse = function(){};
+        expect(url).to.contain('/user/' + mockIssueId + '/response');
+        expect(params).to.eql(undefined);
+        expect(method.getResponse.headers.Authorization).to.eql('Token');
+        expect(method.getResponse.method).to.eql('GET');
+        done();
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.getIssueResponse(mockIssueId, function(err, result) {
+        expect(err).to.eql(undefined);
+      });
+    });
+
+    it('returns an error from the server', function(done) {
+      function mockResource(url, params, method) {
+        this.getResponse = function(body, onLoad, onError) {
+          expect(body).to.eql(undefined);
+          onError('Error');
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.getIssueResponse(mockIssueId, function(err, result) {
+        expect(err).to.eql('Error');
+        done();
+      });
+    });
+
+    it('returns no error when succesful put', function(done) {
+      function mockResource(url, params, method) {
+        this.getResponse = function(body, onLoad, onError) {
+          expect(body).to.eql(undefined);
+          var toBody = {
+            issue_id: mockIssueId,
+            emotional_response: mockResponseValue,
+          };
+          onLoad(toBody);
+        };
+      }
+      var subject = new IssueService(mockResource, mockAuth);
+      subject.getIssueResponse(mockIssueId, function(err, result) {
+        expect(err).to.eql(undefined);
+        expect(result.issue_id).to.eql(mockIssueId);
+        expect(result.emotional_response).to.eql(mockResponseValue);
+        done();
+      });
+    });
+  });
+});
