c436cefb13d34c802913e9a9dbe149d9197a91b7
topgiter <boazt2t@gmail.com>
2016-01-21 19:17:53 +0100

    Added issue model

---
+++ src/models/issue.js
@@ -0,0 +1,17 @@
+function Issue(options) {
+  if (!options) {
+    return this;
+  }
+  this.bill_id = options.bill_id;
+  this.comment = options.comment;
+  this.article_link = options.article_link;
+  this.article_title = options.article_title;
+  this.article_img = options.article_img;
+  this.timestamp = options.timestamp;
+  this.author_id = options.author_id;
+  this.author_first_name = options.author_first_name;
+  this.author_last_name = options.author_last_name;
+  this.author_img_url = options.author_img_url || '//cdn.placeavote.com/img/profile/profile-picture.png';
+}
+
+module.exports = Issue;
---
+++ test/Models/issue.js
@@ -0,0 +1,90 @@
+var Issue = require("../../src/models/issue.js");
+var expect = require("chai").expect;
+
+describe("Issues", function(){
+  it("Issue to have a bill id property", function() {
+    var options = {
+      bill_id: "hr2-114"
+    };
+    var subject = new Issue(options);
+    expect(subject.bill_id).to.eql("hr2-114");
+  });
+
+  it("Issue to have comment property", function() {
+    var options = {
+      comment: "Comment Body goes here"
+    };
+    var subject = new Issue(options);
+    expect(subject.comment).to.eql("Comment Body goes here");
+  });
+
+  it("Issue to have article link property", function() {
+    var options = {
+      article_link: "http://medium.com/somethinginteresting"
+    };
+    var subject = new Issue(options);
+    expect(subject.article_link).to.eql("http://medium.com/somethinginteresting");
+  });
+
+  it("Issue to have article title property", function() {
+    var options = {
+      article_title: "interesting Article"
+    };
+    var subject = new Issue(options);
+    expect(subject.article_title).to.eql("interesting Article");
+  });
+
+  it("Issue to have article image property", function() {
+    var options = {
+      article_img: "http://medium.com/img/101"
+    };
+    var subject = new Issue(options);
+    expect(subject.article_img).to.eql("http://medium.com/img/101");
+  });
+
+  it("Issue to have timestamp property", function() {
+    var options = {
+      timestamp: "123213213"
+    };
+    var subject = new Issue(options);
+    expect(subject.timestamp).to.eql("123213213");
+  });
+
+  it("Issue to have author id property", function() {
+    var options = {
+      author_id: "002"
+    };
+    var subject = new Issue(options);
+    expect(subject.author_id).to.eql("002");
+  });
+
+  it("Issue to have author first name property", function() {
+    var options = {
+      author_first_name: "bob"
+    };
+    var subject = new Issue(options);
+    expect(subject.author_first_name).to.eql("bob");
+  });
+
+  it("Issue to have author last name property", function() {
+    var options = {
+      author_last_name: "pop"
+    };
+    var subject = new Issue(options);
+    expect(subject.author_last_name).to.eql("pop");
+  });
+
+  it("Issue to have article image url property", function() {
+    var options = {
+      author_img_url: "http://img.com"
+    };
+    var subject = new Issue(options);
+    expect(subject.author_img_url).to.eql("http://img.com");
+  });
+
+  it("Issue to have default article image url if not provided", function() {
+    var options = {};
+    var subject = new Issue(options);
+    expect(subject.author_img_url).to.eql("//cdn.placeavote.com/img/profile/profile-picture.png");
+  });
+});
