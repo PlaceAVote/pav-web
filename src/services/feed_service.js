5c9592704530f54a2a50382c706c1027b5f33654
topgiter <boazt2t@gmail.com>
2016-01-22 17:06:20 +0100

    Added feed service and factory

---
+++ src/services/feed_service.js
@@ -0,0 +1,41 @@
+var BillSummary = require('../models/bill_summary.js');
+var config = require('../config/endpoints.js');
+var Issue = require('../../src/models/issue.js');
+var FeedResponseFactory = require('../factories/feed_response_factory.js');
+
+function FeedService($resource, authService, userService) {
+  var getFeed = function(callback) {
+    var token = authService.getAccessToken();
+    if (!token) {
+      callback('Token is needed to get feed');
+      return;
+    }
+
+    var onLoad = function(response) {
+      try {
+        var results = {
+          feed: FeedResponseFactory.getResponses(response.results),
+        };
+        return callback(undefined, results);
+      }
+      catch (e) {
+        return callback(e);
+      }
+    };
+
+    var onError = function(err) {
+      callback(err);
+    };
+
+    var url = config.feed.endpoint;
+    config.methods.get.headers.Authorization = authService.getAccessToken();
+    var request = new $resource(url, undefined, {getFeed: config.methods.get});
+    request.getFeed(undefined, onLoad, onError);
+  };
+  return {
+    getFeed: getFeed,
+  };
+}
+
+module.exports = FeedService;
+
