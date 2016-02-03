6cf2b5403e37ca90e2bb9e78ffb86669cc5d97b9
atostudios <https://atostudios@github.com>
2016-02-03 07:36:02 +0000

    added msd-elastic autoresize to the repo for the post textarea

--- src/controllers/issues_controller.js
+++ src/controllers/issues_controller.js
@@ -1,6 +1,6 @@
 function IssuesController($scope, $rootScope) {
   this.rs = $rootScope;
-  
+  this.post = 'hello';
 };
 
 module.exports = IssuesController
--- src/web-app.js
+++ src/web-app.js
@@ -71,7 +71,8 @@ var preloaderDirective = require('./directives/preloader.js');
 var Facebook = require('./integrations/facebook.js');
 var slider = require('angularjs-slider');
 var draggable = require('angular-ui-tree');
-var app = angular.module('pavApp', [require('angular-route'), require('angular-animate'), require('angular-resource'), require('angular-sanitize'), 'pavDirectives', 'rzModule', 'ui.tree']);
+var textarea = require('angular-elastic');
+var app = angular.module('pavApp', [require('angular-route'), require('angular-animate'), require('angular-resource'), require('angular-sanitize'), 'pavDirectives', 'rzModule', 'ui.tree', 'monospaced.elastic',]);
 
 
 app.config(['$routeProvider', function($routeProvider) {
