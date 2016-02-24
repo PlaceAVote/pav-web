bcc64c6d18789d3a5c3d9d44b993ff811218ecb4
johnervine@hotmail.com <johnervine@hotmail.com>
2016-02-24 13:27:29 +0000

    API Endpoint changes to fix SSL issue in development environment.  New Route53 DNS entries created for API endpoints in development are now reflected in the configuration.

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
-var CONGRESS = 'http://pav-congress-api-515379972.us-east-1.elb.amazonaws.com:8080';
-var USER = 'http://pav-user-api-924234322.us-east-1.elb.amazonaws.com:8080';
-var NOTIFICATION = 'ws://pav-user-api-924234322.us-east-1.elb.amazonaws.com:8080';
-var VOTES = 'http://pav-vote-api-143877429.us-east-1.elb.amazonaws.com:8080';
-var SEARCH = 'http://pav-search-api-1175034802.us-east-1.elb.amazonaws.com:8080';
+var CONGRESS = 'http://congressapidev.placeavote.com';
+var USER = 'http://userapidev.placeavote.com';
+var NOTIFICATION = 'ws://userapidev.placeavote.com';
+var VOTES = 'http://voteapidev.placeavote.com';
+var SEARCH = 'http://searchapidev.placeavote.com';
 var FACEBOOKAPPID = '1686805824874862';
 var MANDRILAPIKEY = 'wIcrhjbexBF4E0vspS7t9w';
 
