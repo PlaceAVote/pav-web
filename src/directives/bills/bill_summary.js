297aadc376ae6b118180520e25628a9da0f330cb
atostudios <https://atostudios@github.com>
2016-03-29 13:39:26 +0100

    added link to pdf version of bill

--- src/directives/bills/bill_summary.js
+++ src/directives/bills/bill_summary.js
@@ -2,5 +2,10 @@ module.exports = function($location) {
   return {
     restict: 'E',
     templateUrl: 'partials/bills/bill_summary.html',
+    link: function(scope, el, attr) {
+      scope.openPdf = function() {
+        window.open(scope.$parent.bill.body.billData.last_version.urls.pdf);
+      }
+    },
   };
 };
