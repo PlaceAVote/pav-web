5679e8b98c2451043a679ad22dccbd3499d39154
atostudios <https://atostudios@github.com>
2016-04-05 15:24:02 +0100

    added hash bang to shared links

--- src/directives/issue.js
+++ src/directives/issue.js
@@ -86,8 +86,8 @@ module.exports = function($location, issueService, facebook, $window) {
           if ($location.$$host === 'placeavote.com') {
             rootHref = 'https://www.placeavote.com';
           }
-          scope.issueLocationFacebook = rootHref + '/#/issue/' + scope.issue.short_issue_id;
-          scope.issueLocation = encodeURIComponent(rootHref + '/#/issue/') + scope.issue.short_issue_id;
+          scope.issueLocationFacebook = rootHref + '/#!/issue/' + scope.issue.short_issue_id;
+          scope.issueLocation = encodeURIComponent(rootHref + '/#!/issue/') + scope.issue.short_issue_id;
         }
       });
 
