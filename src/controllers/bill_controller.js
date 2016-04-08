82dec251567005cac83ce7bbd5aac908d7e40b01
atostudios <https://atostudios@github.com>
2016-04-08 16:39:04 +0100

    removed redundant controller

--- src/controllers/bill_controller.js
+++ src/controllers/bill_controller.js
@@ -184,7 +184,6 @@ BillController.prototype.getLegislator = function(legislator) {
 BillController.prototype.getBill = function(id) {
   var that = this;
   this.billService.getBill(id, function(err, result) {
-    console.log(result);
     if (err) {
       that.error = true;
     } else {
