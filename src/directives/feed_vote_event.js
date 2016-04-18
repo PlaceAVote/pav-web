0e73e7282dfddc3d11bf7e90533b5f62880f91f3
atostudios <https://atostudios@github.com>
2016-04-18 11:21:12 +0100

    style lint changes

--- src/directives/feed_vote_event.js
+++ src/directives/feed_vote_event.js
@@ -1,12 +1,12 @@
 module.exports = function($location) {
-    return {
-        retsrict: 'E',
-        scope: {
-          vote: '='
-        },
-        templateUrl: 'partials/directives/feed_vote_event.html',
-        link: function(scope) {
-          scope.location = $location;
-        }
-    };
+  return {
+    retsrict: 'E',
+    scope: {
+      vote: '=',
+    },
+    templateUrl: 'partials/directives/feed_vote_event.html',
+    link: function(scope) {
+      scope.location = $location;
+    },
+  };
 };
