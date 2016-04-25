ee6d3d8b7ffdf702d560cb7937946eb5c36ad1ad
HistoireDeBabar <p.barber1990@gmail.com>
2016-04-25 17:38:24 +0100

    switch to prerender service

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-var urls = require('./src/config/urls.js');
+var prerenderToken = 'XfWVDqvFXsFXTL8ngxB1';
 
-app.use(require('prerender-node').set('prerenderServiceUrl', urls.PRERENDER).set('protocol', 'https'));
+app.use(require('prerender-node').set('prerenderToken', prerenderToken).set('protocol', 'https'));
 
 app.use('/', express.static(__dirname));
 
 app.listen(port, function() {
-  console.log('listening on ' + port, 'prerender running: ' + urls.PRERENDER);
+  console.log('listening on ' + port, 'prerender running:', prerenderToken);
 });
