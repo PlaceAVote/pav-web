194075d500fb2edd3ac9b05dc63a65aed22bbafd
atostudios <anthony.thomas.oneill@gmail.com>
2016-04-28 15:37:36 +0100

    edits after style lint test

--- src/directives/compile.js
+++ src/directives/compile.js
@@ -4,11 +4,10 @@ module.exports = function($compile, $window, $sce, $sanitize) {
     scope: {
       compile: '<',
     },
-    // terminal: true,
     link: function(scope, element, attrs) {
       scope.window = $window;
       scope.$watch('compile', function(o, n) {
-        if(n) {
+        if (n) {
           element.html(scope.compile);
           $compile(element.contents())(scope);
         }
