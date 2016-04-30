5559a3e809f8f6edffac8d8e71198631f142703a
atostudios <anthony.thomas.oneill@gmail.com>
2016-04-30 10:47:22 +0100

    refactored feed events partial to optimise ng-repeat

--- src/directives/feed_events.js
+++ src/directives/feed_events.js
@@ -8,5 +8,11 @@ module.exports = function() {
       message: '=',
     },
     templateUrl: 'partials/feed/feed_events.html',
+    link: function(scope, el, attr) {
+      // console.log(scope.events);
+      scope.$watch('events', function(n, o) {
+        console.log(scope.events);
+      })
+    }
   };
 };
