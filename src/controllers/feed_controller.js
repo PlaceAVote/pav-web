04709767fae475cdfb9b7298f562d5ac37f922ed
Paul Barber <p.barber1990@gmail.com>
2016-06-03 17:31:57 +0100

    add custom feedback modal

--- src/controllers/feed_controller.js
+++ src/controllers/feed_controller.js
@@ -4,6 +4,7 @@ var title = require('../config/titles.js');
 var BillSummary = require('../models/bill_summary.js');
 var Issue = require('../models/issue.js');
 var SubFeedController = require('./sub_feed_controller.js');
+var Feedback = require('../models/feedback_feed_event.js');
 
 FeedController = function($scope, $location, userService, billService, authService, feedService, $rootScope, $timeout, searchService) {
   AuthorizeController.authorize({error: '/', authorizer: authService, location: $location});
@@ -43,6 +44,8 @@ FeedController = function($scope, $location, userService, billService, authServi
         ],
     }),
   };
+  // Prepopulate with a feedback event.
+  this.categories.all.push([new Feedback({ link: 'http://goo.gl/forms/UMEmQll6XSpeIy4G2' })]);
 
   this.categories.discovery.selectedCategory = this.categories.discovery.categories.trends;
   this.selectedCategory = this.categories.all;
