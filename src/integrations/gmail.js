8c07b1a9b6889f34dee6ff4ba88f524f7ec9b63b
Paul Barber <p.barber1990@gmail.com>
2016-06-05 09:54:31 +0100

    start gmail integration

--- src/integrations/gmail.js
+++ src/integrations/gmail.js
@@ -1,6 +1,10 @@
 var GMAIL_SRC = 'https://apis.google.com/js/client.js';
-var importScript = (function(oHead) {
+var SCOPES = ['https://www.googleapis.com/auth/contacts.readonly'];
+var CLIENT_ID = '669186541269-rhcqafutbk80ffvshmupbq0sjkdvhfae.apps.googleusercontent.com';
+var googleApi;
 
+// Loads the script on page load and sets the gapi vairable;
+var importScript = (function(oHead) {
   function loadError(oError) {
     throw new URIError('The script ' + oError.target.src + ' is not accessible.');
   }
@@ -13,13 +17,68 @@ var importScript = (function(oHead) {
     oHead.appendChild(oScript);
     oScript.src = sSrc;
   }
-
 })(document.head || document.getElementsByTagName('head')[0]);
 
-module.exports = {
-  load: function(callback) {
-    importScript(GMAIL_SRC, function() {
-      callback(gapi);
+importScript(GMAIL_SRC, function() {
+  // Goodle Api has loaded
+  googleApi = gapi;
+});
+
+function loadContacts(callback) {
+  console.log('in action');
+  console.log(googleApi);
+  var request = googleApi.client.gmail.users.labels.list({
+    'userId': 'me',
+  });
+
+  request.execute(function(resp) {
+    console.log(resp);
+    callback(resp);
+  });
+}
+
+function loadGmailApi(action, callback) {
+  googleApi.client.load('contacts', 'v3', function() {
+    console.log('actioned');
+    action(callback);
+  });
+}
+
+function handleAuthResult(authResult, action, callback) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // AuthorizeDiv.style.display = 'none';
+    loadGmailApi(action, callback);
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
+  }
+}
+
+function authorize(action, callback) {
+  console.log('start', googleApi);
+  if (!googleApi) {
+    return setTimeout(function() {
+      console.log('auth');
+      authorize(action, callback);
+    }, 1000);
+  }
+  console.log('hello');
+  googleApi.auth.authorize({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    immediate: false,
+  }, function(resp) {
+      console.log('world');
+      console.log('RESP', resp);
+      handleAuthResult(resp, action, callback);
     });
+}
+
+
+module.exports = {
+  loadContacts: function(callback) {
+    authorize(loadContacts, callback)
   },
 };
--- src/web-app.js
+++ src/web-app.js
@@ -1,9 +1,8 @@
 var config = require('./config/urls.js');
-var GMAIL = require('./integrations/gmail.js');
-var gapi;
-GMAIL.load(function(google) {
-  console.log('GMAIL', google);
-  gapi = google;
+var gmail = require('./integrations/gmail.js');
+
+gmail.loadContacts(function(response) {
+  console.log(response);
 });
 
 console.log('%cPlaceavote', 'background: #543594; color: #ffffff; padding: 1px 3px; border-radius: 3px; font-size: 12px;font-family: sans-serif; margin-left: calc(100% - 70px);');
