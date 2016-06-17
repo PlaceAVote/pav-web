fab99831fba69b22645c647fb40b7f132e6f8a4e
Paul Barber <p.barber1990@gmail.com>
2016-06-17 16:19:47 +0100

    correct styling

--- src/services/open_graph_service.js
+++ src/services/open_graph_service.js
@@ -1,33 +1,33 @@
 var config = require('../config/endpoints.js');
 
 /**
-* getData implementation.
+* GetData implementation.
 * @param request
 * @param url - The URL for the open graph data.
 * @param callback
 */
-function getData(request, url, callback) {
+function getData(Request, url, callback) {
   if (!url) {
     return callback(new Error('No URL defined'));
   }
   var onLoad = function(data) {
     return callback(null, data);
-  }
+  };
   var onError = function(err) {
     return callback(err, null);
-  }
+  };
   var endpoint = config.openGraph.scrape(url);
-  var req = new request(endpoint, undefined, { data: config.methods.get });
+  var req = new Request(endpoint, undefined, { data: config.methods.get });
   req.data(undefined, onLoad, onError);
 }
 
 /**
  * Open Graph Service Interface.
  */
-var openGraphService = function(request){
+var openGraphService = function($request) {
   return {
     getData: function(url, callback) {
-      return getData(request, url, callback);
+      return getData($request, url, callback);
     },
   };
 };
