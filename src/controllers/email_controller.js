1ae7d773ed36a365dbd40015d8ba4446a59ee6cc
Paul Barber <p.barber1990@gmail.com>
2016-06-22 11:08:21 +0100

    update email controller with service call

--- src/controllers/email_controller.js
+++ src/controllers/email_controller.js
@@ -5,10 +5,11 @@ var contact = function() {
   };
 };
 
-function EmailController(google) {
+function EmailController(google, emailService) {
+  this.google = google;
+  this.emailService = emailService;
   this.mesage = '';
   this.contacts = [];
-  this.google = google;
   this.googleContacts = {};
   this.inputContact = contact();
 }
@@ -18,10 +19,14 @@ EmailController.prototype.addInput = function() {
   this.inputContact = contact();
 };
 
+/**
+ * Fetches contacts from the google api.
+ */
 EmailController.prototype.fetchGmailEmails = function() {
   var that = this;
   this.google.getContacts(function(err, contacts) {
     if (err) {
+      // Set user error feedback?
       return;
     }
     // Map the contacts in a hash
@@ -45,5 +50,21 @@ EmailController.prototype.pushValue = function(value) {
   });
 };
 
-module.exports = EmailController;
+/*
+ * Server Interaction to send messages to users.
+ */
+EmailController.prototype.sendMessages = function() {
+  var params = {
+    contacts: this.contacts,
+  };
+  if (this.message) {
+    params.message = this.message;
+  }
+  this.emailService.sendMessageToMany(params, function(err) {
+    if (err) {
+      // Set error message for user?
+    }
+  });
+};
 
+module.exports = EmailController;
